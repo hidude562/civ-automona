e377af34587079eb42d34886b8f3ebe4481ef37c
Nathan M <82677882+hidude562@users.noreply.github.com>
2025-12-15 13:28:35 -0500

    set_script solution for tileData

---
+++ game/game/script/CellData.gd
@@ -0,0 +1,5 @@
+# CellData.gd - Create this as a separate script file
+extends Resource
+
+var orientation: int = 0
+var times_moved: int = 0
--- game/game/script/simulation.gd
+++ game/game/script/simulation.gd
@@ -1,120 +1,123 @@
+# Your TileMapLayer script
 extends TileMapLayer
+
+# Per-cell data storage: Vector2i -> CellData
+var cell_data_map: Dictionary = {}
+
+# Preload the script for set_script usage
+var CellDataScript = preload("res://script/CellData.gd")
+
+func get_cell_data(coords: Vector2i) -> Resource:
+	if not cell_data_map.has(coords):
+		var data = Resource.new()
+		data.set_script(CellDataScript)
+		cell_data_map[coords] = data
+	return cell_data_map[coords]
+
+func remove_cell_data(coords: Vector2i) -> void:
+	cell_data_map.erase(coords)
+
+func move_cell_data(from: Vector2i, to: Vector2i) -> void:
+	if cell_data_map.has(from):
+		cell_data_map[to] = cell_data_map[from]
+		cell_data_map.erase(from)
+
 # Orientation 
 func get_cell_orientation(coords: Vector2i) -> int:
-	var orientation: int
-	
-	# Get cell orientation using object metadata
-	var cell: TileData = get_cell_tile_data(coords)
-	if cell.has_meta("orientation"):
-		orientation = cell.get_meta("orientation")
-	else:
-		cell.set_meta("orientation", 0)
-		orientation = 0
-	
-	return orientation
+	return get_cell_data(coords).orientation
 
-func increment_orientation(obj: Object):
-	var orientation = obj.get_meta("orientation")
-	obj.set_meta("orientation", (orientation + 1) % 6)
+func set_cell_orientation(coords: Vector2i, value: int) -> void:
+	get_cell_data(coords).orientation = value
 
-# movement (Equal to NESW)
-func get_cell_movement(coords: Vector2i) -> Vector2:
+func increment_orientation(coords: Vector2i) -> void:
+	var data = get_cell_data(coords)
+	data.orientation = (data.orientation + 1) % 6
+
+# Movement (Equal to NESW for hex)
+func get_cell_movement(coords: Vector2i) -> Vector2i:
 	var orientation = get_cell_orientation(coords)
-	var movement = Vector2i()
-
 	match orientation:
-		0:
-			movement = Vector2i(1, 0)
-		1:
-			movement = Vector2i(0, 1)
-		2:
-			movement = Vector2i(-1, 1)
-		3:
-			movement = Vector2i(-1, 0)
-		4:
-			movement = Vector2i(-1, -1)
-		5:
-			movement = Vector2i(0, -1)
-			
+		0: return Vector2i(1, 0)
+		1: return Vector2i(0, 1)
+		2: return Vector2i(-1, 1)
+		3: return Vector2i(-1, 0)
+		4: return Vector2i(-1, -1)
+		5: return Vector2i(0, -1)
+	return Vector2i()
 
-	return movement
-
-func tile_iterate(coords: Vector2i, old_tile_map: TileMapLayer, new_tile_map: TileMapLayer):
+func tile_iterate(coords: Vector2i, old_tile_map: TileMapLayer, new_tile_map: TileMapLayer, new_data_map: Dictionary):
 	var tile_type = old_tile_map.get_cell_source_id(coords)
-	var cell_data = old_tile_map.get_cell_tile_data(coords)
-
+	
 	match tile_type:
 		# Source tile
 		0:
 			var new_person_spot_delta: Vector2i = get_cell_movement(coords)
 			var new_person_spot: Vector2i = new_person_spot_delta + coords
 			
-			# Put new person there with orientation
+			# Put new person there
 			new_tile_map.set_cell(new_person_spot, 2, Vector2i(0, 0))
 			
-			# Copy orientation of this
-			var new_person_data = new_tile_map.get_cell_tile_data(new_person_spot)
-			new_person_data.set_meta("orientation", cell_data.get_meta("orientation"))
+			# Create new cell data with copied orientation
+			var new_data = Resource.new()
+			new_data.set_script(CellDataScript)
+			new_data.orientation = get_cell_data(coords).orientation
+			new_data_map[new_person_spot] = new_data
 			
-			increment_orientation(cell_data)
+			# Increment source orientation
+			increment_orientation(coords)
+			
 		# Building
 		1:
 			pass
+			
 		# Person
 		2:
 			var movement: Vector2i = get_cell_movement(coords)
 			var new_expected_tile: Vector2i = coords + movement
 			
-			# Cut it
+			# Cut it from new map
 			new_tile_map.set_cell(coords, -1)
 			
-			# If empty space..
 			var new_expected_tile_source_id = old_tile_map.get_cell_source_id(new_expected_tile)
 			match new_expected_tile_source_id:
 				# Empty space
 				-1:
-					# Increment times moved
-					if cell_data.has_meta("times_moved"):
-						var times_moved = cell_data.get_meta("times_moved")
-						cell_data.set_meta("times_moved", times_moved + 1)
-					else:
-						cell_data.set_meta("times_moved", 1)
+					var current_data = get_cell_data(coords)
+					current_data.times_moved += 1
 					
-					# Move
+					# Move tile
 					new_tile_map.set_cell(new_expected_tile, 2, Vector2i(0, 0))
 					
-					# Copy data
-					var new_tile_map_data = new_tile_map.get_cell_tile_data(new_expected_tile)
-					
-					new_tile_map_data.set_meta("times_moved", cell_data.get_meta("times_moved"))
-					new_tile_map_data.set_meta("orientation", cell_data.get_meta("orientation"))
-					
-					print(cell_data.get_meta("orientation"))
-					
+					# Move data to new position
+					new_data_map[new_expected_tile] = current_data
 					
 				# Building logic
 				1:
 					pass
-				# Default = remove
+				# Collision with person = remove
 				2:
 					pass
-			
-			
-			
+
 func iterate() -> void:
 	var used_cells = get_used_cells()
 	var copy_tilemap: TileMapLayer = self.duplicate(true)
+	var new_data_map: Dictionary = {}
 	
 	for cell in used_cells:
-		tile_iterate(cell, self, copy_tilemap)
+		tile_iterate(cell, self, copy_tilemap, new_data_map)
 	
 	self.set_tile_map_data_from_array(copy_tilemap.tile_map_data)
 	
-	# Calculate additional tiles that must be calculated
-	# (Empty tiles that may be created in the case of there being 4 people surrounding a tile, which creates a new structure)
-	# (Functions as a set)
-	var additional_to_calculate: Dictionary = {}
+	# Update cell data map with new positions
+	# Keep source tiles (type 0) data, replace everything else
+	var sources_to_keep: Dictionary = {}
+	for cell in get_used_cells():
+		if get_cell_source_id(cell) == 0:
+			sources_to_keep[cell] = cell_data_map.get(cell)
+	
+	cell_data_map = new_data_map
+	cell_data_map.merge(sources_to_keep, true)
 
 func _input(event):
-	if event.is_action_pressed("ui_accept"):  # Space or Enter
+	if event.is_action_pressed("ui_accept"):
 		iterate()
